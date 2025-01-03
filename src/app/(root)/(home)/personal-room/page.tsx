7136e57660701e322674eff3e870acfd985afdf5
Abiral Jain <abiral2724@gmail.com>
2025-01-03 22:51:23 +0530

    navbar and sidebar done

---
+++ src/app/(root)/(home)/personal-room/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Personalroom = () => {
+  return (
+    <div>Personalroom</div>
+  )
+}
+
+export default Personalroom
---
+++ src/app/(root)/(home)/previous/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const previous
+ = () => {
+  return (
+    <div>previous
+        
+    </div>
+  )
+}
+
+export default previous
