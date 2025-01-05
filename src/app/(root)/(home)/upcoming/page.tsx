4524db09e4dd8f0bbc23a99557c8828a2d13299c
Abiral Jain <abiral2724@gmail.com>
2025-01-06 00:02:07 +0530

    upcoming page done

--- src/app/(root)/(home)/upcoming/page.tsx
+++ src/app/(root)/(home)/upcoming/page.tsx
@@ -1,8 +1,15 @@
+import CallList from '@/components/CallList'
 import React from 'react'
 
 const Upcoming = () => {
   return (
-    <div>Upcoming</div>
+    <div>
+        <section className='flex size-full flex-col gap-10 text-white'>
+            <h1 className='text-2xl font-bold'>Upcoming Meetings</h1>
+
+            <CallList type="upcoming"></CallList>
+        </section>
+    </div>
   )
 }
 
